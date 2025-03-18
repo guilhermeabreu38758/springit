cdaceb1e76b717e83e1f3a423f4f060a550f6519
guilherme <guilherme@gsa-pc>
2025-03-18 10:44:25 +0000

    Initial Commit for our Spring Boot reddit clone

---
+++ src/test/java/pt/caso/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.caso.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
